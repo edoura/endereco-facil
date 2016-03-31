3b8d17d3d6470984cc4240073629b12bca95c1f2
Eduardo Moura <edoura@gmail.com>
2016-03-31 17:57:42 -0300

    iniciando o maps

---
+++ mapa.js
@@ -0,0 +1,27 @@
+var geocoder;
+var map;
+var marker;
+
+function initialize() {
+    var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
+    var options = {
+        zoom: 5,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("mapa"), options);
+
+    geocoder = new google.maps.Geocoder();
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+    });
+
+    marker.setPosition(latlng);
+}
+
+$(document).ready(function () {
+    initialize();
+});
